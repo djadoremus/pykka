8b6e6792adf952aff3d35427cecd5eef4249cee7
Adoremus, Daniel Joseph V <dj@adoremus.ph>
2014-08-15 00:36:54 -0400

    fixed reply message attribute

--- pykka.py
+++ pykka.py
@@ -12,10 +12,10 @@ import json
 import ast
 import urllib
 
-client_id="62eb59d295fa6e9dd81c636944c457956e8586147d732502767a374275ed9785"
-secret_key="e6886536ec7563e0032c7fb81835b356151190fbb6498e2ff9c8cc0811cdcf7c"
-shortcode="292902673"
-reply_message=urllib.urlencode({"message":"Message Received"})
+client_id=""
+secret_key=""
+shortcode=""
+reply_message="Message Received!"
 
 app = Flask(__name__)
 app.debug = True
@@ -33,7 +33,6 @@ def sendSMS(mobile_number, message):
 		"client_id":client_id,
 		"secret_key":secret_key
 	}
-	#payload = "message_type=SEND&mobile_number="+mobile_number+"&shortcode="+shortcode+"&message_id="+message_id+"&message="+message+"&client_id="+client_id+"&secret_key="+secret_key
 	payload = urllib.urlencode(payloadDict)
 	r = requests.post("https://post.chikka.com/smsapi/request", data=payload)
 	print("---calling post chikka " + r.url)
@@ -70,14 +69,14 @@ def receiveSMS():
 			"mobile_number":formJSON['mobile_number'],
 			"shortcode":formJSON['shortcode'],
 			"message_id":os.urandom(16).encode('hex'),
-			"message":"Message Received",
+			"message":reply_message,
 			"request_id":formJSON['request_id'],
 			"request_cost":"FREE",
 			"client_id":client_id,
 			"secret_key":secret_key
 		}
 		payload = urllib.urlencode(payloadDict)
-		#payload = "message_type=REPLY&mobile_number="+formJSON['mobile_number']+"&shortcode="+formJSON['shortcode']+"&message_id="+os.urandom(16).encode('hex')+"&"+reply_message+"&request_id="+formJSON['request_id']+"&request_cost=FREE&client_id="+client_id+"&secret_key="+secret_key
+		print("---payload " , payload)
 		r = requests.post("https://post.chikka.com/smsapi/request", data=payload)
 		print("---calling post chikka " + r.url)
 
